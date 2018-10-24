3b64f52617dd3fdb133ea99dc77a9aa0090bccb0
vvdhout <valentijnvdh@gmail.com>
2018-10-24 18:34:46 +0200

    Setting up truffle so it can run tests; adding some functionality to the StarNotary contract.

--- smart_contracts/truffle.js
+++ smart_contracts/truffle.js
@@ -1,18 +1,23 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+const mnemonic = "food tone alien diet typical promote never salon crumble rely pretty other"; 
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  },
+  rinkeby: {
+  	provider: function() {
+  		return new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/b8b9c6970c744cb4b10c4ef53c9bbcc6')
+  	},
+  	network_id: 4,
+  	gas: 4500000,
+  	gasPrice: 10000000000
+  }
 };
